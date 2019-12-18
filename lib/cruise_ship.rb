5f3afadec2112f723fb1e300a71b9a6ffb200740
Latricia Nickelberry <tricianickelberry@gmail.com>
2019-12-18 13:36:32 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,7 +5,12 @@
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
 # }
+ 
+winner
+end
 
-def select_winner(passengers)
-  # add the code snippet here!
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
